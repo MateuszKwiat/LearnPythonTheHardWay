538024abaa029d3fa452b334367e8aadb090f7f9
Mateusz Kwiatkowski <mateusz.kwiat316@gmail.com>
2022-08-12 18:04:06 +0200

    ex1-7

---
+++ ex5.py
@@ -0,0 +1,18 @@
+name = 'Mateusz'
+age = 20
+height = 185
+weight = 82
+eyes = 'Green'
+teeth = 'White'
+hair = 'Brown'
+
+print(f"let's talk about {name}.")
+print(f"He's {height} cm tall.")
+print(f"He's {weight} kg heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffee.")
+
+# this line is tricky, try to get it exactly right
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight} I get {total}.")
